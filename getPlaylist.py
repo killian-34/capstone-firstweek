203337b507ede6320b59d9f7a21c5f05fd0692e9
Jackson Killian <jakillian.34@gmail.com>
2017-08-24 18:03:41 -0400

    Pulling spotify library and sorting

--- getPlaylist.py
+++ getPlaylist.py
@@ -15,6 +15,7 @@ token = util.prompt_for_user_token(username, scope)
 if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks()
+
     for item in results['items']:
         track = item['track']
         print track['name'] + ' - ' + track['artists'][0]['name']
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import argparse
 import tone_analyzer as t
+from PyLyrics import PyLyrics
+
+
 
 def print_tone_dict(tone_dict):
 	for category in tone_dict["document_tone"]["tone_categories"]:
@@ -9,13 +12,25 @@ def print_tone_dict(tone_dict):
 		print
 
 parser = argparse.ArgumentParser(description = "Get tone from text using IBM Watson")
-parser.add_argument("textfile", type = str, help = "")
+parser.add_argument("-f","--songfile", type = str, help = "")
+parser.add_argument("-s", "--songname", type = str, help = "")
+parser.add_argument("-a", "--artist", type = str, help = "")
+
 args = parser.parse_args()
 
-textfile = open(args.textfile,'r')
-tone_text = textfile.read()
-textfile.close()
-tone_dict = t.get_tone_for_text(tone_text)
+text_we_want = ""
+if (args.songname):
+	lyrics = PyLyrics.getLyrics(args.artist, args.songname)
+	print lyrics
+	text_we_want = lyrics
+
+else:
+	textfile = open(args.songfile,'r')
+	tone_text = textfile.read()
+	textfile.close()
+	text_we_want = tone_text
+
+tone_dict = t.get_tone_for_text(text_we_want)
 
 print_tone_dict(tone_dict)
 
