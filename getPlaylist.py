9a9c271d2940f34674fa2996c7aa9d212df05757
amad881 <amadh881@gmail.com>
2017-08-24 18:05:54 -0400

    Read from playlist

--- getPlaylist.py
+++ getPlaylist.py
@@ -2,10 +2,12 @@ import sys
 import spotipy
 import spotipy.util as util
 
-scope = 'user-library-read'
+scope = 'playlist-modify-private'
 
 if len(sys.argv) > 1:
-    username = sys.argv[1]
+    uri = sys.argv[1]
+    username = uri.split(':')[2]
+    playlist_id = uri.split(':')[4]
 else:
     print "Usage: %s username" % (sys.argv[0],)
     sys.exit()
@@ -14,9 +16,14 @@ token = util.prompt_for_user_token(username, scope)
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
+    results = sp.user_playlist_tracks(username, playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
 else:
     print "Can't get token for", username
+
+for item in tracks:
+    track = item['track']
+    print track['name'] + ' - ' + track['artists'][0]['name']
